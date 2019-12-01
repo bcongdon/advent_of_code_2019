658ac7a11cc7ce5d346b8922ce094db5f3071155
Benjamin Congdon <bencongdon96@gmail.com>
2019-12-01 08:14:24 -0800

    Day1 in Rust

---
+++ Day1-9/day1.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn fuel_req(m: &i32) -> i32 {
+    let f = (m / 3) - 2;
+    if f > 0 {
+        f
+    } else {
+        0
+    }
+}
+
+fn fuel_req_2(m: &i32) -> i32 {
+    let mut f = fuel_req(m);
+    if f > 0 {
+        f += fuel_req_2(&f);
+    }
+    f
+}
+
+pub fn main() {
+    let lines = read_lines("1.txt").expect("file not found");
+    let modules = lines
+        .map(|x| x.unwrap().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let part1: i32 = modules.iter().map(fuel_req).sum();
+    println!("Part 1: {}", part1);
+
+    let part2: i32 = modules.iter().map(fuel_req_2).sum();
+    println!("Part 2: {}", part2);
+}
