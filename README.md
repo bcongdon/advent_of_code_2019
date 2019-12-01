89cd8f91b4796c5dc248393d435dcb5568e8b526
Benjamin Congdon <bencongdon96@gmail.com>
2019-11-30 20:01:38 -0800

    Make readme generator more easy to use

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# advent_of_code_2018
-🎅 My solutions to the 2018 "Advent of Code"
+# advent_of_code_2019
+🎅 My solutions to the 2019 "Advent of Code"
+
+
+## Solutions
+
+
--- readme_generator.py
+++ readme_generator.py
@@ -33,33 +33,55 @@ extensions = {
     "rs": "Rust",
 }
 
+TEMPLATE = """# advent_of_code_2019
+🎅 My solutions to the 2019 "Advent of Code"
+
+
+## Solutions
+
+{}"""
+
+
+def files_for_day(day):
+    fdr = next(x[1] for x in folders if x[0] > i)
+    if not os.path.isdir(fdr):
+        return
+
+    for fn in os.listdir(fdr):
+        try:
+            name, ext = os.path.splitext(fn)
+            day = next(
+                int(name[i:]) for i in range(len(name)) if name[i:].isdigit()
+            )
+        except StopIteration:
+            pass
+        # print("Coundn't parse: %s" % fn)
+        except ValueError:
+            pass
+        else:
+            if ext[1:] in extensions and day == i:
+                yield os.path.join(fdr, fn)
+
+
+def pretty_extension_name(path):
+    _, ext = os.path.splitext(path)
+    return extensions[ext[1:]]
+
+
 if __name__ == "__main__":
     out = ""
     soln_temp = "    * [{}]({})\n"
     symbols = itertools.cycle(list(day_labels))
     for i in range(1, 26):
-        fdr = next(x[1] for x in folders if x[0] > i)
-        if not os.path.isdir(fdr):
-            continue
+        header = "* Day {}:  {}\n".format(i, next(symbols))
+        solution_files = files_for_day(i)
 
-        temp = "* Day {}:  {}\n".format(i, next(symbols))
-        sol_for_today = False
-        for fn in os.listdir(fdr):
-            try:
-                name, ext = fn.split(".")
-                day = next(
-                    int(name[i:]) for i in range(len(name)) if name[i:].isdigit()
-                )
-            except StopIteration:
-                pass
-            # print("Coundn't parse: %s" % fn)
-            except ValueError:
-                pass
-            else:
-                if ext in extensions and day == i:
-                    full = os.path.join(fdr, fn)
-                    temp += soln_temp.format(extensions[ext], full)
-                    sol_for_today = True
-        if sol_for_today:
-            out += temp
-    print(out)
+        tmp = ''
+        for s in solution_files:
+            sol_type = pretty_extension_name(s)
+            tmp += soln_temp.format(sol_type, s)
+
+        if tmp != '':
+            out += header
+            out += tmp
+    print(TEMPLATE.format(out))
